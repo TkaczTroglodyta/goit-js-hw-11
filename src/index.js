a7f312c01799cf887a0494b9114f035d50e1bce1
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-09 23:33:46 +0200

    feat: code update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './sass/main.scss';
 import { simpltbox } from './js/simpltbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  btnMorePics: document.querySelector('.more-pics'),
+};
