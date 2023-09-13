61f94ccdd1c3f2de0795000cebe06e4cef262d30
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-13 18:37:57 +0200

    feat: minor & major updates

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const refs = {
 const PixabayApi = new PixabayApi();
 
 refs.searchForm.addEventListener('submit', onSearch);
+refs.morePicsBtn.addEventListener('click', onMorePics);
 
 function onSearch(e) {
   e.preventDefault();
@@ -28,6 +29,11 @@ function onSearch(e) {
   fetchGallery();
 }
 
+function onMorePics() {
+  PixabayApi.addPage();
+  fetchGallery();
+}
+
 async function fetchGallery() {
   refs.morePicsBtn.classList.add('is-hidden');
   const { hits, totalHits } = await PixabayApi.fetchGallery();
@@ -38,4 +44,50 @@ async function fetchGallery() {
     refs.morePicsBtn.classList.add('is-hidden');
     return;
   }
+  onGalleryCreate(hits);
+
+  onScreen = PixabayApi.page < Math.ceil(totalHits / 40);
+
+  if (onScreen !== true) {
+    Notify.failure(
+      `We're sorry, but you've reached the end of search results.`
+    );
+    refs.morePicsBtn.classList.add('is-hidden');
+    return;
+  }
+  if (onScreen === true) {
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+    refs.morePicsBtn.classList.remove('is-hidden');
+    return;
+  }
+}
+
+function onGalleryCreate(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="pict-card">
+      <a href="${largeImageURL}">
+        <img class="pict-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item"><strong>Likes</strong>${likes}</p>
+        <p class="info-item"><strong>Views</strong>${views}</p>
+        <p class="info-item"><strong>Comments</strong>${comments}</p>
+        <p class="info-item"><strong>Downloads</strong>${downloads}</p>
+      </div>
+    </div>`;
+      }
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  PixabayApi.refresh();
 }
