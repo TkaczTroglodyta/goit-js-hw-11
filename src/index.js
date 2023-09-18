a5e490325d2bd90d9778a724be11cd1e023217d7
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-09-18 22:57:04 +0200

    feat: create & import new files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
+import { refs } from './js/refs';
 import PixabayApi from './js/pixabay-api';
-import { picsGallery } from './js/sltbox';
+import { createGallery } from './js/create-gallery';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-const refs = {
-  searchForm: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
-  morePicsBtn: document.querySelector('.more-pics'),
-};
 
 let onScreen = 0;
 const getResults = new PixabayApi();
@@ -43,7 +38,7 @@ async function fetchGallery() {
     return;
   }
 
-  onGalleryCreate(hits);
+  createGallery(hits);
 
   onScreen = getResults.page < Math.ceil(totalHits / 40);
 
@@ -60,48 +55,6 @@ async function fetchGallery() {
     refs.morePicsBtn.classList.remove('is-hidden');
     return;
   }
-}
-
-function onGalleryCreate(elements) {
-  const markup = elements
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<div class="pict-card">
-    <a href="${largeImageURL}">
-      <img class="pict-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-    <div class="pict-card__info">
-      <p class="pict-card__txt">
-        <strong>Likes</strong>
-        ${likes}
-      </p>
-      <p class="pict-card__txt">
-        <strong>Views</strong>
-        ${views}
-      </p>
-      <p class="pict-card__txt">
-        <strong>Comments</strong>
-        ${comments}
-      </p>
-      <p class="pict-card__txt">
-        <strong>Downloads</strong>
-        ${downloads}
-      </p>
-    </div>
-    </div>`;
-      }
-    )
-    .join('');
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
-  picsGallery.refresh();
 }
 
 refs.searchForm.addEventListener('submit', onSearch);
